f52bb89606a0873cf8aab2a201e48ced762b0cad
Sai Sharana Shivesh <shivesh27@Shivs-Kakashi.local>
2024-12-07 15:28:31 +0530

    empty base

--- svelte.config.js
+++ svelte.config.js
@@ -15,9 +15,8 @@ const config = {
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter(),
 		paths: {
-			base: '/svelte-app'
-		},
-		trailingSlash: 'always'
+			base: ''
+		}
 	}
 };
 
