f2b912cbb156be29ff59b8a04023a28d79148599
Sai Sharana Shivesh <shivesh27@Shivs-Kakashi.local>
2024-12-07 15:14:32 +0530

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<nav>
+    <ul>
+        <li><a href="/">main</a></li>
+        <li><a href="/counter">Counter</a></li>
+    </ul>
+</nav>
+
+<!-- header -->
+
+<main class="container">
+    <slot/>
+</main>
+
+<!-- footer -->
---
+++ src/routes/counter/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+    let count = 0
+</script>
+
+<h1>counter: {count}</h1>
+<button on:click={() => count += 1}> inc </button>
+<button on:click={() => count -= 1}> dec </button>
